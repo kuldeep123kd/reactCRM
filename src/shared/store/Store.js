c29efbee221b96f7ca0a23e4e718651c4d450189
Kuldeep Shakya <kuldeep.shakya.kd@gmail.com>
2021-02-05 15:03:39 +0530

    some more changes

--- src/shared/store/Store.js
+++ src/shared/store/Store.js
@@ -5,23 +5,8 @@ import { Context } from './Context';
 
 export const Store = props => {
 
-  const [open, setOpen] = React.useState(false);
-  const [isLoading, setIsLoading] = React.useState(true);
-  const [isPlanet, setIsPlanet] = React.useState(false);
-  const [isError, setIsError] = React.useState(false);
-
-  const handleClose = () => {
-    setOpen(false);
-  }
-
-  const handleToggle = () => {
-    setOpen(!open);
-  };
-
   return (
     <Context.Provider value={{
-      handleClose, handleToggle, open, isPlanet, 
-      setIsPlanet, isLoading, isError
       }}> 
       {props.children}
     </Context.Provider>
